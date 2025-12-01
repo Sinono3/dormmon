c18140a18c98952d118feccdbfcb81f70bb0f7e7
MaiaFigue <maiabetha04@gmail.com>
2025-12-01 14:25:53 +0800

    Modify expense page (only ui)

--- acn_ui/pages/expense.py
+++ acn_ui/pages/expense.py
@@ -1,5 +1,7 @@
 import tkinter as tk
 import ttkbootstrap as ttk
+from ttkbootstrap.dialogs import Messagebox
+from api import APIError
 
 # *================ EXPENSE PAGE =====================*
 class ExpensePage(ttk.Frame):
@@ -24,14 +26,14 @@ class ExpensePage(ttk.Frame):
 
     ttk.Label(amountFrame, text="Amount:", font=("Helvetica", 12)).pack(anchor="w")
 
-    self.amount = ttk.StingVar()
+    self.amount = ttk.StringVar()
     ttk.Entry(amountFrame, textvariable=self.amount).pack(fill="x")
 
     #amount buttons
     buttonRow = ttk.Frame(amountFrame)
     buttonRow.pack(pady=5)
     for val in (1, 10, 100):
-      ttk.Button(buttonRow, text=f"+{val}", width=6).pack(side="left", padx=5)
+      ttk.Button(buttonRow, text=f"+{val}", width=6, command= lambda v=val: self.addAmount(v)).pack(side="left", padx=5)
     
     #category 
     categoryFrame = ttk.Frame(self)
@@ -49,24 +51,21 @@ class ExpensePage(ttk.Frame):
     ttk.Label(splitFrame, text="Split between:", font=("Helvetica", 12)).pack(anchor="w")
 
     self.splitCheckFrame = ttk.Frame(splitFrame)
-    self.splitCheckFrame.pack(anchor="w", pady=5)
+    self.splitCheckFrame.pack(side="left", pady=5)
 
     self.checkVars = {}
 
-    self.msg = ttk.Label(self, text="", font=("Helvetica", 11))
-    self.msg.pack(pady=5)
-
-    #bottom buttons
-    btnFrame = ttk.Frame(self)
-    btnFrame.pack(pady=10)
-
-    ttk.Button(btnFrame, text="Save Expense", bootstyle="success").pack(side="left", padx=10)
+    ttk.Button(splitFrame, text="Save Expense", bootstyle="success", command=self.saveExpense).pack(side="right", padx=10)
     
-    #Button
+    #Buttons
+    clrBut = ttk.Button(buttonRow, text="CLR", width=6, command=self.clearAmount)
+    clrBut.pack(side="left", padx=5)
+
     self.backBut = ttk.Button(self, image=controller.backIm, command=controller.go_back)
     self.backBut.place(relx=1.0, x=-5, y=5, anchor="ne")
   
   def onShow(self):
+    #Load names
     userNames = [u["name"] for u in self.controller.users]
     self.payerBox["values"] = userNames
 
@@ -75,12 +74,14 @@ class ExpensePage(ttk.Frame):
     elif userNames:
       self.payerBox.set(userNames[0])
 
+    #Load categories
     catNames = [c["name"] for c in self.controller.categories]
     self.categoryBox["values"] = catNames
 
     if catNames:
       self.categoryBox.set(catNames[0])
 
+    #Reload split checkboxes
     for w in self.splitCheckFrame.winfo_children():
       w.destroy()
 
@@ -88,13 +89,66 @@ class ExpensePage(ttk.Frame):
 
     for u in self.controller.users:
       var = tk.BooleanVar(value=True)
-      ttk.Checkbutton(self.splitCheckFrame, text=u["name"], variable=var).pack(anchor="w")
+      ttk.Checkbutton(self.splitCheckFrame, text=u["name"], variable=var).pack(side="left", padx=5)
       self.checkVars[u["id"]] = var
 
+  def addAmount(self, value):
+    current = self.amount.get()
+    try:
+      current = float(current) if current else 0
+    except ValueError:
+      current = 0
+    self.amount.set(str(current + value))
+
+  def saveExpense(self):
+    payerName = self.payerBox.get()
+    categoryName = self.categoryBox.get()
+
+    try:
+      amount = float(self.amount.get())
+      if amount <= 0:
+          raise ValueError
+    except:
+      Messagebox.show_error("Invalid amount", "Amount must be a positive number.")
+      return
+
+    if not payerName:
+      Messagebox.show_error("Error", "Select a payer.")
+      return
+
+    if not categoryName:
+      Messagebox.show_error("Error", "Select a category.")
+      return
+
+    payerId = next(u["id"] for u in self.controller.users if u["name"] == payerName)
+
+    categoryId = self.controller.get_category_id(categoryName)
+
+    splitUsersId = [uid for uid, var in self.checkVars.items() if var.get() == 1]
+    if not splitUsersId:
+      Messagebox.show_error("Error", "Select at least one member to split with.")
+      return
     
-    self.msg.config(text="")
+    payload = {
+      "payer_id": payerId,
+      "amount": amount,
+      "category_id": categoryId,
+      "split_with": splitUsersId
+    }
 
+    try:
+      self.controller.api.create_expense(payload)
+    except APIError as exc:
+      MemoryError.show_error("Unable to save", str(exc))
+      return
+    
+    self.controller.refresh_users()
+    Messagebox.ok("Expense saved succesfully!")
 
-
-
-
+    #Reset ui
+    self.amount.set("0")
+    for var in self.checkVars.values():
+      var.set(True)
+  
+  def clearAmount(self):
+    self.amount.set("")
